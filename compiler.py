a52d1fc4bc6f105395d0f867e081cbb2d45fdd3c
ShayanEmzed <shayan80sama@gmail.com>
2022-05-05 18:58:40 +0430

    fix

--- compiler.py
+++ compiler.py
@@ -1,5 +1,5 @@
 """
-Shayan Mohammadizadh 98102273
+Shayan Mohammadizadeh 98102273
 Nona Ghazizadeh 98171007
 """
 
@@ -7,15 +7,17 @@ Nona Ghazizadeh 98171007
 from Parser.parser import Parser
 # from Scanner.new_version_scanner import Scanner
 
-
 parser = Parser()
-res = parser.parse()
-print(res)
+parser.parse()
+with open('parse_tree.txt', 'w') as f:
+    f.write(parser.parse_tree)
+
+with open('syntax_errors.txt', 'w') as f:
+    if parser.syntax_error == '':
+        f.write('There is no syntax error.')
+    else:
+        f.write(parser.syntax_error)
+
 # scanner = Scanner('input.txt')
 # while True:
-#     print(scanner.get_next_token())
-
-# print(parser.table['Relop'])
-
-with open('parse_tree.txt', 'w') as f:
-    f.write(res)
+#     print(f'{scanner.get_next_token()} {scanner.lineno}')
