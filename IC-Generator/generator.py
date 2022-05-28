a748d6e487a80c4658166a1172305cdbc841bec6
ShayanEmzed <shayan80sama@gmail.com>
2022-05-28 22:12:11 +0430

    feat: ready for merge with added symbol table

---
+++ IC-Generator/generator.py
@@ -0,0 +1,15 @@
+
+
+
+class Generator:
+
+    def __init__(self):
+        self.semantic_stack = []
+
+    def generate_code(self, action_symbol):
+        func = {
+            'update_method_addr': self.update_method_address
+        }
+
+
+    def update_method_addr(self):
